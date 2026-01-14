3355a699fc859c54d18a56e8586b7cfa29054ccd
ruixuanp1122 <ruixuanp@uci.edu>
2026-01-13 19:35:49 -0800

    Hw2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,46 @@ public class Player : MonoBehaviour
 {
     [Header("Jump Settings")]
     [SerializeField] private float _jumpForce = 8f;
+
     [Header("Ground Check")]
     [SerializeField] private Transform _groundCheck;
     [SerializeField] private float _groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask _groundLayer;
+    private Rigidbody2D _rb;
+    private bool _isGrounded;
     // Start is called before the first frame update
-   private void Start()
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            Jump();
+        }
+    }
+    private void FixedUpdate()
+    {
+        // Check if player is grounded
+        _isGrounded = Physics2D.OverlapCircle(
+            _groundCheck.position,
+            _groundCheckRadius,
+            _groundLayer
+        );
+    }
+    private void Jump()
+    {
+        _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (_groundCheck == null) return;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(_groundCheck.position, _groundCheckRadius);
     }
 }
